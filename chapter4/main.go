5b851ec281425287369c0bb3ce4ef9f6566d85cf
Christine Le <christine.le244@gmail.com>
2020-01-23 12:49:08 -0500

    Naming variables and scope

---
+++ chapter4/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+/*	var x string
+	x = "first"
+	fmt.Println(x)
+	x = "second"
+	fmt.Println(x)
+
+
+	var x string
+	x = "first"
+	fmt.Println(x)
+	x += "second"
+	fmt.Println(x)
+*/
+	
+	x := "Hello World"
+	fmt.Println(x)
+}
+
+func f() {
+	fmt.Println(x)
+}
